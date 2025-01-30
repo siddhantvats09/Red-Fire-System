f8dfbc15b1dde77fa99a78bd0b8eea67111cdfab
Siddhant Vats <siddhantvats840@gmail.com>
2025-01-30 17:37:41 +0530

    page done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,231 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <div className="">
+        <div className="text-[32px] md:text-[48px] h-[110px] lg:h-[120px] flex items-center justify-center font-bold bg-[#00A17B] text-white">
+          <h1 className="text-center">Welcome to Red Fire Systems Private Limited</h1>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="px-[20px] md:px-[70px] ">
+          <h2 className="text-[24px] md:text-[36px] text-[#00A17B] text-center mt-10 font-semibold">
+            Your Trusted Partner in Fire Safety & Engineering Solutions
+          </h2>
+          <p className="text-[#00795D] mt-8 text-[18px] md:text-[24px] text-justify ">
+            At Red Fire Systems Private Limited, we are dedicated to ensuring
+            safety through state-of-the-art fire protection and engineering
+            solutions. Established in 2012, we have earned a strong reputation
+            for delivering high-quality fire safety equipment, system
+            installations, and maintenance services that meet national and
+            international safety standards.
+          </p>
+          <p className="text-[#00795D] mt-8 text-[18px] md:text-[24px] text-justify">
+            Fire safety is a critical aspect of any infrastructure, whether
+            industrial, commercial, or residential. Our mission is to provide
+            advanced fire detection, suppression, and prevention systems that
+            mitigate risks, protect lives, and safeguard property. With a team
+            of experienced fire safety experts, engineers, and technicians, we
+            work relentlessly to ensure that our clients receive the best fire
+            protection solutions tailored to their specific needs.
+          </p>
+        </div>
+        <hr className="w-full h-1 bg-[#00A17B] mt-10"></hr>
+        <div className="px-[20px] md:px-[70px] flex justify-center flex-col gap-8 my-12">
+          <div className="bg-[#00A17B] text-white rounded-lg shadow-lg w-full  p-8">
+            <h2 className="text-[20px] md:text-[40px] font-bold text-center">
+              ABOUT US
+            </h2>
+            <p className="mt-6 text-[18px] md:text-[28px] text-justify font-normal">
+              As a leading name in the fire safety industry, Red Fire Systems
+              Private Limited offers a comprehensive range of fire protection
+              solutions designed to address the unique challenges of modern
+              businesses and institutions.
+            </p>
+            <p className="mt-6 text-[18px] md:text-[28px] text-justify font-normal">
+              We specialize in the design, manufacturing, supply, installation,
+              and maintenance of fire safety systems. Our expertise extends
+              across multiple sectors, ensuring that our solutions are
+              customized to meet the specific requirements of various
+              industries.
+            </p>
+            <p className="mt-6 text-[18px] md:text-[28px] text-justify font-normal">
+              Our team is well-versed in fire safety compliance, regulatory
+              standards, and emerging technologies, allowing us to deliver
+              innovative and effective fire protection solutions. We believe in
+              a proactive approach to fire safety, helping businesses implement
+              preventive measures that minimize fire risks before they become
+              major threats.
+            </p>
+          </div>
+        </div>
+        <div className="px-[20px] md:px-[70px] flex justify-center flex-col gap-8 my-12">
+          <div className="bg-[#044a99c1] text-white rounded-lg shadow-lg w-full  p-8">
+            <h2 className="text-[20px] md:text-[40px] font-bold text-center">
+              Our Key Offerings{" "}
+            </h2>
+            <p className="mt-6 text-[16px] md:text-[24px] text-center font-normal">
+              We provide a wide range of fire safety solutions that cater to
+              businesses of all sizes.<br className="hidden lg:block"></br> Our
+              core services include:
+            </p>
+            <hr className="w-full h-1 bg-[#ffffff] my-10"></hr>
+
+            <div className="md:mt-6 mt-3">
+              <h3 className="md:my-6 my-3 text-[18px] md:text-[36px]  font-bold">
+                Fire Detection & Alarm Systems :
+              </h3>
+              <ul className=" text-[14px] md:text-[24px] font-medium">
+                <li className="mb-3">
+                  ✅ Automatic Fire Detection Systems – Early warning systems
+                  for rapid fire detection.
+                </li>
+                <li className="mb-3">
+                  ✅ Smoke & Heat Detectors – Advanced sensors to detect fire
+                  hazards before they escalate.
+                </li>
+                <li className="mb-3">
+                  ✅ Addressable & Conventional Fire Alarm Systems – Tailored
+                  solutions for different types of infrastructure.
+                </li>
+              </ul>
+              <div className="flex justify-center">
+                <hr className="w-[70%]  h-[1.7px] bg-[#8BADFC] my-3 md:my-10"></hr>
+              </div>
+            </div>
+            <div className="md:mt-6 mt-3">
+              <h3 className="md:my-6 my-3 text-[18px] md:text-[36px]  font-bold">
+                Fire Suppression Systems :
+              </h3>
+              <ul className=" text-[14px] md:text-[24px] font-medium">
+                <li className="mb-3">
+                  ✅ Gas-Based Fire Suppression Systems – CO2, FM-200, and
+                  Inergen gas solutions for critical areas.
+                </li>
+                <li className="mb-3">
+                  ✅ Water Mist & Foam Suppression Systems – Effective systems
+                  for industrial and commercial spaces.
+                </li>
+                <li className="mb-3">
+                  ✅ Kitchen Fire Suppression Systems – Specialized protection
+                  for restaurants and commercial kitchens.
+                </li>
+              </ul>
+              <div className="flex justify-center">
+                <hr className="w-[70%]  h-[1.7px] bg-[#8BADFC] my-3 md:my-10"></hr>
+              </div>
+            </div>
+            <div className="md:mt-6 mt-3">
+              <h3 className="md:my-6 my-3 text-[18px] md:text-[36px]  font-bold">
+                Fire Extinguishers & Safety Equipment :
+              </h3>
+              <ul className=" text-[14px] md:text-[24px] font-medium">
+                <li className="mb-3">
+                  ✅ Portable & Fixed Fire Extinguishers – ABC, CO2, Foam, and
+                  Dry Powder extinguishers.
+                </li>
+                <li className="mb-3">
+                  ✅ Fire Blankets, Hydrants & Hose Reels – Essential fire
+                  protection accessories.
+                </li>
+                <li className="mb-3">
+                  ✅ Emergency Exit Signs & Safety Gear – Ensuring a safe
+                  evacuation during emergencies.
+                </li>
+              </ul>
+              <div className="flex justify-center">
+                <hr className="w-[70%]  h-[1.7px] bg-[#8BADFC] my-3 md:my-10"></hr>
+              </div>
+            </div>
+            <div className="mb-0">
+              <h3 className="md:my-6 my-3 text-[18px] md:text-[36px]  font-bold">
+                Installation, Inspection & Maintenance Services :
+              </h3>
+              <ul className=" text-[14px] md:text-[24px] font-medium">
+                <li className="mb-3">
+                  ✅ Fire System Installation – Expert setup of fire alarms,
+                  suppression systems, and sprinklers.
+                </li>
+                <li className="mb-3">
+                  ✅ Annual Maintenance Contracts (AMC) – Regular inspections to
+                  ensure system reliability.
+                </li>
+                <li className="mb-3">
+                  ✅ Fire Drills & Safety Training – Educating employees and
+                  staff on fire safety best practices.
+                </li>
+              </ul>
+              <div className="flex justify-center">
+                <hr className="w-[70%]  h-[1.7px] bg-[#8BADFC] my-3 md:my-10"></hr>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="px-[20px] md:px-[70px] flex justify-center flex-col gap-8 my-12">
+          <div className="bg-[#046c99c4] text-white rounded-lg shadow-lg w-full  p-8">
+            <h2 className="text-[20px] md:text-[40px] font-bold text-center">
+              Why Choose Red Fire Systems Private Limited?
+            </h2>
+            <p className="md:mt-10 mt-6 text-[18px] md:text-[28px]  font-semibold">
+              Expert Team – Led by Amit Kumar Bhardwaj (CEO) and Sonia Sharma,
+              our team consists of skilled professionals with years of
+              experience in fire safety solutions.
+            </p>
+
+            <div className="md:mt-10 mt-6 text-[18px] md:text-[26px]  font-normal">
+              <p className="mb-3">
+                ✔ Certified & Compliant – We adhere to the highest safety
+                regulations, ensuring reliable and efficient fire protection
+                systems.
+              </p>
+              <p className="mb-3">
+                ✔ Customer-Focused Approach – We provide customized fire safety
+                solutions based on industry-specific requirements.
+              </p>
+              <p className="mb-3">
+                {" "}
+                ✔ Affordable & Scalable Solutions – Our services cater to
+                businesses of all sizes, from small offices to large industrial
+                facilities.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="px-[20px] md:px-[70px] flex justify-center flex-col gap-8 my-12">
+          <div className="bg-[#04995dda] text-white rounded-lg shadow-lg w-full  p-8">
+            <h2 className="text-[20px] md:text-[40px] font-bold text-center  underline ">
+              COMPANY INFO
+            </h2>
+
+            <div className="md:mt-10 mt-6 text-[18px] md:text-[30px] flex justify-between md:flex-row flex-col  font-semibold">
+              <p className="">0124-220-0010</p>
+              <p className="">Info@redfiresystems.com</p>
+              <p className=""> Employee size 300-500</p>
+            </div>
+            <div className="my-6 md:my-10">
+              <p className="mb-6 text-[16px] md:text-[24px]  font-normal text-justify">
+                RED FIRE SYSTEMS PRIVATE LIMITED (CIN: U29253HR2012PTC046377) is
+                a Private company incorporated on 28 Dec 2012. It is classified
+                as Non-government company and is registered at Registrar of
+                Companies, Delhi.
+              </p>
+              <p className="mb-6 text-[16px] md:text-[24px]  font-normal text-justify">
+                FIRE SYSTEMS PRIVATE LIMITED&apos;s NIC code is 2925 (which is
+                part of its CIN). As per the NIC code, it is inolved in
+                Manufacture of machinery for food, beverage and tobacco
+                processing.
+              </p>
+              <p className="mb-6 text-[16px] md:text-[24px]  font-normal text-justify">
+                Directors of RED FIRE SYSTEMS PRIVATE LIMITED are AMIT KUMAR
+                BHARDWAJ, and SONIA SHARMA.{" "}
+              </p>
+              <p className="mb-6 text-[16px] md:text-[24px]  font-normal text-justify">
+                RED FIRE SYSTEMS PRIVATE LIMITED&apos;s Corporate Identification
+                Number (CIN) is U29253HR2012PTC046377 and its registration
+                number is 46377. Users may contact RED FIRE
+                SYSTEMS PRIVATE LIMITED
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
